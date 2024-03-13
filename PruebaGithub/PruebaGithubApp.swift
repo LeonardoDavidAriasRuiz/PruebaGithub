f27de9377f15e6db71a06e259814e776971a7888
LeonardoDavidAriasRuiz <126028045+LeonardoDavidAriasRuiz@users.noreply.github.com>
2024-03-12 20:52:47 -0600

    Second commit

---
+++ PruebaGithub/PruebaGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  PruebaGithubApp.swift
+//  PruebaGithub
+//
+//  Created by Leonardo on 12/03/24.
+//
+
+import SwiftUI
+
+@main
+struct PruebaGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
